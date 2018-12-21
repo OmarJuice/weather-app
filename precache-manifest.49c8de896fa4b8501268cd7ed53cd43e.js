202cbd35fbd67c3ae7133587971d637f80830b7f
Omar Juice <ojameer1@gmail.com>
2018-12-21 12:45:11 -0500

    Updates

---
+++ precache-manifest.49c8de896fa4b8501268cd7ed53cd43e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "c41b2afccca8d2ff4482",
+    "url": "/weather-app/static/js/main.c41b2afc.chunk.js"
+  },
+  {
+    "revision": "a1e1392c5f332f5cf235",
+    "url": "/weather-app/static/js/1.a1e1392c.chunk.js"
+  },
+  {
+    "revision": "e9ace5a00922bfbb33c40ed74eb3ec53",
+    "url": "/weather-app/index.html"
+  }
+];
